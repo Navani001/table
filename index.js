012de1e23354707bc4ce6ab88162517ec942f400
navani_hk <navaneetha2006krishnan@gmail.com>
2024-04-15 16:22:25 +0530

    checkin and checkout done

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var mysql = require('mysql');
 const moment = require("moment");
 const bodyParser = require('body-parser');
 const { render } = require('ejs');
-app.set("view engine","ejs")
 
+app.set("view engine","ejs")
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 var con = mysql.createConnection(
   {
@@ -15,7 +16,6 @@ var con = mysql.createConnection(
   password: "Navan@123",
   database:"ps"
 });
-
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
@@ -23,14 +23,13 @@ con.connect(function(err) {
 app.use(express.static(path.join(__dirname,"public")))
 app.get('/',(req,res)=>{
   res.render("index",{element:""})
-  
 });
 var name;
 var id;
 var user_info={"name":name,"id":id}
 var CHECKIN_VALUE={}
-app.post("/user",(req,res)=>{
-  
+var CHECKOUT_VALUE={}
+app.get("/user",(req,res)=>{
   var date=moment().format("MMDDYYYY")
   var time=moment().format("HH:mm:ss")
 
@@ -42,26 +41,42 @@ app.post("/user",(req,res)=>{
         if (err) throw err;})
         con.query("alter table CHECKIN add column o"+ date.toString()+" time",function (err, result) {
           if (err) throw err;})
-      }
-      con.query("UPDATE CHECKIN SET i"+ date.toString()+"= '"+time+"' WHERE id="+id,function (err, result) {
-      if (err) throw err;})
-      console.log("value added")
+      }  
+      
     con.query("select * from CHECKIN where id="+id,function (err, result) {
     
-        console.log(Object.keys(result[0]).length)
         for (var key in result[0]){
-          if(!(key=="ID" || key[0]=="o"))
+          
+          if(key[0]=="i")
           {
-          console.log( key + ": " + result[0][key]);
-        
-        CHECKIN_VALUE[key] = result[0][key];
+          CHECKIN_VALUE[key] = result[0][key];
           }
-      }
-      console.log(CHECKIN_VALUE)
-        res.render("user",{element:CHECKIN_VALUE,user:user_info});
-      
+          if(key[0]=="o")
+          {
+            CHECKOUT_VALUE[key] = result[0][key];
+          }
+    }
+    console.log("page updated")
+        res.render("user",{element:CHECKIN_VALUE,user:user_info,element2:CHECKOUT_VALUE});
     })
-})})
+})
+})
+app.post('/user',
+	(req, res) => {
+    
+    var date=moment().format("MMDDYYYY")
+    var time=moment().format("HH:mm:ss")
+     if(req.body.username=="checkout")
+     {
+      con.query("UPDATE checkin SET o"+date+" = '"+time+"' WHERE id="+id)
+      console.log("checked out");
+     }
+     else{
+      con.query("UPDATE checkin SET i"+date+" = '"+time+"' WHERE id="+id)
+      console.log("checked in");
+     }
+     res.redirect('./user');
+	});
 app.post('/',(req,res)=>{
     name=req.body.username;
     var pass=req.body.userpassword;
@@ -70,27 +85,10 @@ app.post('/',(req,res)=>{
         id=result[0].id
         user_info.id=id
         user_info.name=name
-        console.log(result);
-        console.log(result[0].password)
-        console.log(pass)
         if(result[0].password==pass)
         {
           console.log("login sucessfully")
-          con.query("select * from CHECKIN where id="+id,function (err, result) {
-    
-            console.log(Object.keys(result[0]).length)
-            for (var key in result[0]){
-              if(key!="ID")
-              {
-              console.log( key + ": " + result[0][key]);
-            
-            CHECKIN_VALUE[key] = result[0][key];
-              }
-          }
-          console.log(CHECKIN_VALUE)
-            res.render("user",{element:CHECKIN_VALUE,user:user_info});
-          
-        })
+          res.redirect('./user');
         }
         else
         {
