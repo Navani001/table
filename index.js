18ea6531c72d65533f41b56c95b4c3397aa63524
navani_hk <navaneetha2006krishnan@gmail.com>
2024-04-18 09:51:26 +0530

    first commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var con = mysql.createConnection(
   host: "localhost",
   user: "root",
   password: "Navan@123",
-  database:"ps"
+  database:"try"
 });
 con.connect(function(err) {
   if (err) throw err;
@@ -30,70 +30,16 @@ var user_info={"name":name,"id":id}
 var CHECKIN_VALUE={}
 var CHECKOUT_VALUE={}
 app.get("/user",(req,res)=>{
-  var date=moment().format("MMDDYYYY")
-  var time=moment().format("HH:mm:ss")
 
-    con.query("SHOW COLUMNS FROM `checkin` LIKE 'i"+ date.toString()+"'", function (err, result) {
-     
-      if (result[0]==undefined)
-      {
-        con.query("alter table CHECKIN add column i"+ date.toString()+" time",function (err, result) {
-        if (err) throw err;})
-        con.query("alter table CHECKIN add column o"+ date.toString()+" time",function (err, result) {
-          if (err) throw err;})
-      }  
-      
-    con.query("select * from CHECKIN where id="+id,function (err, result) {
-    
-        for (var key in result[0]){
-          
-          if(key[0]=="i")
-          {
-          CHECKIN_VALUE[key] = result[0][key];
-          }
-          if(key[0]=="o")
-          {
-            CHECKOUT_VALUE[key] = result[0][key];
-          }
-    }
-    console.log("page updated")
-        res.render("user",{element:CHECKIN_VALUE,user:user_info,element2:CHECKOUT_VALUE});
-    })
+  con.query("select * from student",function(err,result){
+    res.render("user",{contacts:result})
+    console.log(result)
+  })
 })
-})
-app.post('/user',
-	(req, res) => {
-    
-    var date=moment().format("MMDDYYYY")
-    var time=moment().format("HH:mm:ss")
-     if(req.body.username=="checkout")
-     {
-      con.query("UPDATE checkin SET o"+date+" = '"+time+"' WHERE id="+id)
-      console.log("checked out");
-     }
-     else{
-      con.query("UPDATE checkin SET i"+date+" = '"+time+"' WHERE id="+id)
-      console.log("checked in");
-     }
-     res.redirect('./user');
-	});
 app.post('/',(req,res)=>{
     name=req.body.username;
     var pass=req.body.userpassword;
-    con.query("select * from student where name='"+name+"'", function (err, result) {
-        if (err) throw err;
-        id=result[0].id
-        user_info.id=id
-        user_info.name=name
-        if(result[0].password==pass)
-        {
-          console.log("login sucessfully")
-          res.redirect('./user');
-        }
-        else
-        {
-          res.render("index",{element:"Invalid password or user name"});
-        }
-      }); 
-})
+    var email=req.body.email;
+    con.query("insert into student (name,password,email) values('"+name+"',"+"'"+pass+"',"+"'"+email+"')")
+    res.redirect('/user');})
 app.listen(2500)
